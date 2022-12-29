2da570a9cfa1061ffad9584046a04d50b732e66b
David <davidms93@outlook.es>
2022-12-29 10:22:25 +0100

    Subida 0.1

---
+++ js/script.js
@@ -0,0 +1,109 @@
+document.getElementById("btn-menu").addEventListener("click", mostrarMenu);
+document.getElementById("back-menu").addEventListener("click", ocultarMenu);
+
+nav = document.getElementById("nav");
+backgroundMenu = document.getElementById("back-menu");
+
+function mostrarMenu() {
+    nav.style.right = "0px";
+    backgroundMenu.style.display = "block";
+}
+
+function ocultarMenu() {
+    nav.style.right = "-250px";
+    backgroundMenu.style.display = "none";
+}
+
+
+/*----------------------TYPEWRITER-------------------------*/
+
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.09em solid #4edba0}";
+    document.body.appendChild(css);
+};
+
+
+/*-----------------SWIPER-PROJECTS---------------*/
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+           slidesPerView: 1, 
+        },
+        520: {
+            slidesPerView: 2, 
+         },
+         950: {
+            slidesPerView: 3, 
+         },
+    },
+  });
